42ac467f241fa5220c1da33b8ae28f1101c87f11
HXS482 <2870272575@qq.com>
2023-11-10 21:04:41 +0800

    Site updated: 2023-11-10 21:04:41

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/11/10/我的第一篇博客文章/","2023/11/09/hello-world/","2023/11/10/鸡巴/"];function toRandomPost(){
+var posts=["2023/11/09/hello-world/","2023/11/10/鸡巴/","2023/11/10/我的第一篇博客文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
